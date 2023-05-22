931a1e78aab4c05017750f97e2e29321ec9a03cd
mrleonardobrito <lbritogit@gmail.com>
2023-05-22 14:52:06 -0300

    removing cut from tool cmd

--- lib/utils.bash
+++ lib/utils.bash
@@ -72,7 +72,7 @@ install_version() {
 
 		# TODO: Assert <YOUR TOOL> executable exists.
 		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		tool_cmd="$(echo "$TOOL_TEST")"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
