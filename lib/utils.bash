09a83f192be8038ec3cac45e0b8109f2ce673f83
mrleonardobrito <lbritogit@gmail.com>
2023-05-22 14:56:32 -0300

    troubleshooting utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -60,7 +60,7 @@ download_release() {
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%}"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -68,11 +68,12 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"/bin
 
 		# TODO: Assert <YOUR TOOL> executable exists.
 		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST")"
+		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		chmod +x "$install_path/$tool_cmd"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
